4bfaab1f196e91393201ecf2c639e5bacc924b1a
hthuz <hanggang.20@intl.zju.edu.cn>
2024-07-20 20:25:29 +0800

    issues: strings & comments

--- assignments/PA2/cool.flex.c
+++ assignments/PA2/cool.flex.c
@@ -34,6 +34,9 @@ extern FILE *fin; /* we read from this file */
 
 char string_buf[MAX_STR_CONST]; /* to assemble string constants */
 char *string_buf_ptr;
+char *string_buf_ptr_end;
+bool eof_err_reported = false;
+bool null_err_reported = false;
 
 extern int curr_lineno;
 extern int verbose_flag;
@@ -74,59 +77,160 @@ FALSE   "false"
 TYPEID [A-Z]([a-zA-Z]|[0-9]|_)*
 OBJECTID [a-z]([a-zA-Z]|[0-9]|_)*
 ASSIGN "<-"
-NOT "~"
+NOT "not"
 LE "<="
 
 WHITESPACE (" "|"\n"|"\t")+
 EXPR TRUE|FALSE|STRING|ID|DIGITS
 
+%x COMMENT
+%x STR
 %%
 
  /*
   *  Nested comments
   */
 
+"(*" BEGIN(COMMENT);
+<COMMENT>[^*\n]* {}
+<COMMENT>"*"+[^*)\n]* {}
+<COMMENT>"\n" {curr_lineno++;}
+<COMMENT>"*)" BEGIN(INITIAL);
+<COMMENT><<EOF>> {
+  if (eof_err_reported) {
+    yyterminate();
+  }
+  yylval.error_msg = "EOF in comment"; 
+  eof_err_reported = true;
+  return (ERROR);
+  }
+
+"--"+.*+"\n" {curr_lineno++;}
+"*)" {yylval.error_msg = "Unmatched *)"; return (ERROR);}
+
+"\"" null_err_reported = false,string_buf_ptr = string_buf; string_buf_ptr_end = string_buf_ptr + MAX_STR_CONST; BEGIN(STR);
+<STR>"\"" {
+    BEGIN(INITIAL);
+    *string_buf_ptr = '\0';
+    if (null_err_reported) {
+      yylval.error_msg = "String contains null character";
+      return (ERROR);
+    }
+    yylval.symbol = stringtable.add_string(string_buf);
+    return STR_CONST;
+}
+
+<STR>\n {
+  BEGIN(INITIAL);
+  curr_lineno++;
+  yylval.error_msg = "Unterminated string constant";
+  return ERROR;
+}
+<STR>\0 {
+  null_err_reported = true;
+}
+<STR>"\\0" {
+  if (string_buf_ptr == string_buf_ptr_end) {
+    yylval.error_msg = "String constant too long";
+    return (ERROR);
+  }
+  *string_buf_ptr++ = '0';
+}
+<STR>"\\b" {
+  if (string_buf_ptr == string_buf_ptr_end) {
+    yylval.error_msg = "String constant too long";
+    return (ERROR);
+  }
+  *string_buf_ptr++ = '\b';
+}
+<STR>"\\t" {
+  if (string_buf_ptr == string_buf_ptr_end) {
+    yylval.error_msg = "String constant too long";
+    return (ERROR);
+  }
+  *string_buf_ptr++ = '\t';
+}
+<STR>"\\n" {
+  if (string_buf_ptr == string_buf_ptr_end) {
+    yylval.error_msg = "String constant too long";
+    return (ERROR);
+  }
+  *string_buf_ptr++ = '\n';
+}
+<STR>"\\f" {
+  if (string_buf_ptr == string_buf_ptr_end) {
+    yylval.error_msg = "String constant too long";
+    return (ERROR);
+  }
+  *string_buf_ptr++ = '\f';
+}
+
+<STR>\\(.|\n) {
+  if (string_buf_ptr == string_buf_ptr_end) {
+    yylval.error_msg = "String constant too long";
+    return (ERROR);
+  }
+  if (yytext[1] == '\n') 
+    curr_lineno++;
+  *string_buf_ptr++ = yytext[1];
+}
+
+<STR>[^\0\\\n\"]+ {
+  int i = 0;
+  while (yytext[i]) {
+    if (string_buf_ptr == string_buf_ptr_end) {
+      yylval.error_msg = "String constant too long";
+      return (ERROR);
+    }
+    *string_buf_ptr++ = yytext[i++];
+  }
+
+}
+<STR><<EOF>> {
+  if (eof_err_reported)
+    yyterminate();
+  yylval.error_msg = "EOF in string constant"; 
+  eof_err_reported = true;
+  return (ERROR);
+} 
+
  /*
   *  The multiple-character operators.
   */
 {INT_CONST} { yylval.symbol = inttable.add_string(yytext); return INT_CONST; }
-{TRUE} { yylval.boolean = true; return BOOL_CONST;}
-{FALSE} { yylval.boolean = false; return BOOL_CONST; }
-{TYPEID} { yylval.symbol = idtable.add_string(yytext); return TYPEID; }
+t(?i:rue) { yylval.boolean = true; return BOOL_CONST;}
+f(?i:alse) { yylval.boolean = false; return BOOL_CONST; }
 
-"\n" {
-    curr_lineno += 1;
-}
-" "|"\f"|"\r"|"\t"|"\v" {
-  
-}
+"\n" {curr_lineno += 1;}
+" "|"\f"|"\r"|"\t"|"\v" {}
 
 
  /*
   * Keywords are case-insensitive except for the values true and false,
   * which must begin with a lower-case letter.
   */
-"self" { yylval.symbol = idtable.add_string(yytext); return TYPEID; }
+"self" { yylval.symbol = idtable.add_string(yytext); return OBJECTID; }
 "SELF_TYPE" { yylval.symbol = idtable.add_string(yytext); return TYPEID; }
-{CLASS} {return {CLASS};}
-{ELSE} {return {ELSE};}
-{FI} {return {FI};}
-{IF} {return {IF};}
-{INHERITS} {return {INHERITS};}
-{LET} {return {LET};}
-{LOOP} {return {LOOP};}
-{POOL} {return {POOL};}
-{THEN} {return {THEN};}
-{WHILE} {return {WHILE};}
-{CASE} {return {CASE};}
-{ESAC} {return {ESAC};}
-{OF} {return {OF};}
-{DARROW} { return (DARROW); }
-{NEW} {return {NEW};}
-{ISVOID} {return {ISVOID};}
-{ASSIGN} {return {ASSIGN};}
-{NOT} {return {NOT};}
-{LE} {return {LE};}
+(?i:{CLASS}) {return (CLASS);}
+(?i:{ELSE}) {return (ELSE);}
+(?i:{FI}) {return (FI);}
+(?i:{IF}) {return (IF);}
+(?i:{IN}) {return (IN);}
+(?i:{INHERITS}) {return (INHERITS);}
+(?i:{LET}) {return (LET);}
+(?i:{LOOP}) {return (LOOP);}
+(?i:{POOL}) {return (POOL);}
+(?i:{THEN}) {return (THEN);}
+(?i:{WHILE}) {return (WHILE);}
+(?i:{CASE}) {return (CASE);}
+(?i:{ESAC}) {return (ESAC);}
+(?i:{OF}) {return (OF);}
+(?i:{DARROW}) { return (DARROW); }
+(?i:{NEW}) {return (NEW);}
+(?i:{ISVOID}) {return (ISVOID);}
+(?i:{ASSIGN}) {return (ASSIGN);}
+(?i:{NOT}) {return (NOT);}
+(?i:{LE}) {return (LE);}
 "(" {return 40;}
 ")" {return 41;}
 "*" {return 42;}
@@ -139,8 +243,10 @@ EXPR TRUE|FALSE|STRING|ID|DIGITS
 ";" {return 59;}
 "<" {return 60;}
 "=" {return 61;}
+"@" {return 64;}
 "{" {return 123;}
 "}" {return 125;}
+"~" {return 126;}
 
  /*
   *  String constants (C syntax)
@@ -149,7 +255,8 @@ EXPR TRUE|FALSE|STRING|ID|DIGITS
   *
   */
 
-
+{TYPEID} { yylval.symbol = idtable.add_string(yytext); return TYPEID; }
 {OBJECTID} { yylval.symbol = idtable.add_string(yytext); return OBJECTID; }
+. {yylval.error_msg = yytext; return ERROR;}
 
 %%
--- assignments/PA2/mytest/hello_world.cl
+++
@@ -1,4 +0,0 @@
-class Main inherits IO {
-   main(): SELF_TYPE {
-   };
-};
